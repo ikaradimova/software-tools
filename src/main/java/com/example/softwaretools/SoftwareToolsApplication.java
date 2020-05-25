159a1b4eebc72a0c500123d7984d70e874dc5c32
Irina <ikaradimova@gmail.com>
2020-05-25 10:17:26 +0300

    creating empty maven project

---
+++ src/main/java/com/example/softwaretools/SoftwareToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwaretools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softwaretools/SoftwareToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwaretools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
